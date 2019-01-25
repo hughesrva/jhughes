f48f3422b3c3f99e3cc5c6d736818c50e71de957
skiplodem <hughesrva@gmail.com>
2019-01-24 22:30:58 -0500

    adding updates

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+// var bulma = require("bulma");
+// var bulmaCarousel = require("bulma-carousel");
+
+document.addEventListener(
+  "DOMContentLoaded",
+  function() {
+    var carousels = bulmaCarousel.attach();
+  },
+  false
+);
+
+$(document).ready(function() {
+  $(".navbar-burger").click(function() {
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+
+    var carousels = bulmaCarousel.attach();
+    false;
+  });
+});
