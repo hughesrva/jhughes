082dc4212dad0d90e2088231980a797042c19ef4
skiplodem <hughesrva@gmail.com>
2019-01-26 20:45:26 -0500

    adding final changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,12 @@
-document.addEventListener(
-  "DOMContentLoaded",
-  function() {
-    var carousels = bulmaCarousel.attach();
-  },
-  false
-);
-
 $(document).ready(function() {
   $(".navbar-burger").click(function() {
     // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
     $(".navbar-burger").toggleClass("is-active");
     $(".navbar-menu").toggleClass("is-active");
   });
-  var carousels = bulmaCarousel.attach(); // carousels now contains an array of all Carousel instances
+  $('.carousel').carousel();
+});
+
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true,
 });
